9a941e855e44711536ebacb1bbd9f7e4d339e0e8
snaranjo9969 <36332475+snaranjo9969@users.noreply.github.com>
2019-08-18 17:33:12 -0500

    Update AplicacionCalculadora.java

--- AplicacionCalculadora.java
+++ AplicacionCalculadora.java
@@ -11,6 +11,32 @@ public class AplicacionCalculadora
 {
       public static void main (String[] args)  
   {
-      
+        boolean prueba = false;    
+        Scanner adquirir = new Scanner(System.in);
+        String calculo;
+        double respuesta= 0;
+ 
+        do{
+            String primerdigi;
+            do {
+                System.out.println("\n Escriba el primer numero para calcular ");
+                primerdigi = adquirir.nextLine();
+            } while (!primerdigi.matches("[+-]?[\\d]*[.]?[\\d]+"));
+            double num1 = new Double(primerdigi);
+            double numero1 = Double.parseDouble(primerdigi);
+            
+           
+            do {
+                System.out.println("\n escoja que metodo de calculo desea usar");
+                System.out.println("use el simbolo para escoger \n + = suma \n - = resta \n"
+                        + " x = multiplicacion \n / = division \n * = potencia."
+                        + "\n % = residual");
+            calculo = adquirir.nextLine();
+                if (calculo.equals("+") || calculo.equals("-") || calculo.equals("x") ||
+                    calculo.equals("X") || calculo.equals("/") || calculo.equals("%") ||
+                    calculo.equals("*")) {
+                    prueba = true;
+                }else { prueba = false; }
+            } while (prueba != true);
   }
 }
